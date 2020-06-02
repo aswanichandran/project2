037f984b2f9095f1130622c22517af8c2a45bfbd
aswanichandran <aswani.chandran94@gmail.com>
2020-06-02 20:58:57 +0530

    commit2

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,11 @@
+package org.add;
+
+public class GreensTech {
+   public void greensOmr() {
+	System.out.println("omr");
+}
+   public static void main(String[]args) {
+	   GreensTech g=new GreensTech();
+	   g.greensOmr();
+   }
+}
